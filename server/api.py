3c108b11f1f5d546143dcb1e52d616e3de02ea93
João Negreiros <142504941+joao-victor-wls@users.noreply.github.com>
2024-05-04 14:59:49 -0300

    Adicionando criptografia a senha

--- server/api.py
+++ server/api.py
@@ -1,5 +1,6 @@
 from flask import Flask, jsonify, request
 from database import *
+import bcrypt
 
 class ServerApi:
     def __init__(self, host='127.0.0.1', port=5000):
@@ -14,47 +15,25 @@ class ServerApi:
         def index():
             return 'Sucesso'
 
-        # COMPANIES ==========================================================================================================================
-
-        @self.app.route('/create_company', methods=['POST'])
-        def create_company():
-            # Obtendo os dados do formulário enviado
-            name_company = request.form.get('name_company')
-            email = request.form.get('email')
-
-            try:
-                # Inserindo os dados na tabela COMPANIES
-                cursor = self.connector.connection.cursor()
-                query = ("INSERT INTO COMPANIES (NAME_COMPANY, EMAIL) "
-                         "VALUES (%s, %s)")
-                data = (name_company, email)
-                cursor.execute(query, data)
-                self.connector.connection.commit()
-                cursor.close()
-                
-                return 'Empresa criada com sucesso!'
-            except Exception as err:
-                return f'Erro ao criar empresa: {err}'
-
         # USERS ==========================================================================================================================
 
         @self.app.route('/create_user', methods=['POST'])
         def create_user():
             # Obtendo os dados do formulário enviado
             
-            company_id = request.form.get('company_id')
             complete_name = request.form.get('complete_name')
             email = request.form.get('email')
-            password_hash = request.form.get('password_hash')
+            password_hash = request.form.get('password')
+            password_hash = self.encrypt_password(password_hash)
             access_token = request.form.get('access_token')
             refresh_token = request.form.get('refresh_token')
 
             try:
                 # Inserindo os dados na tabela USERS
                 cursor = self.connector.connection.cursor()
-                query = ("INSERT INTO USERS (COMPANY_ID, COMPLETE_NAME, EMAIL, PASSWORD_HASH, ACCESS_TOKEN, REFRESH_TOKEN) "
-                         "VALUES (%s, %s, %s, %s, %s, %s)")
-                data = (company_id, complete_name, email, password_hash, access_token, refresh_token)
+                query = ("INSERT INTO USERS (COMPLETE_NAME, EMAIL, PASSWORD_HASH, ACCESS_TOKEN, REFRESH_TOKEN) "
+                         "VALUES (%s, %s, %s, %s, %s)")
+                data = (complete_name, email, password_hash, access_token, refresh_token)
                 cursor.execute(query, data)
                 self.connector.connection.commit()
                 cursor.close()
@@ -70,19 +49,34 @@ class ServerApi:
             password = request.form.get('password')
 
             try:
-                # Verificando se o usuário existe e a senha está correta
+                # Obtendo os dados do usuário do banco de dados
                 cursor = self.connector.connection.cursor()
-                query = ("SELECT * FROM USERS WHERE EMAIL = %s AND PASSWORD_HASH = %s")
-                cursor.execute(query, (email, password))
+                query = ("SELECT EMAIL, PASSWORD_HASH FROM USERS WHERE EMAIL = %s")
+                cursor.execute(query, (email,))
                 user = cursor.fetchone()
-                cursor.close()
 
                 if user:
-                    return 'Login bem-sucedido!'
+                    # Obtendo a senha criptografada armazenada no banco de dados
+                    senha_criptografada_armazenada = user[1]
+                    
+                    # Verificando se a senha fornecida corresponde à senha criptografada armazenada
+                    if bcrypt.checkpw(password.encode('utf-8'), senha_criptografada_armazenada.encode('utf-8')):
+                        cursor.close()
+                        return 'Login bem-sucedido!'
+                    else:
+                        cursor.close()
+                        return 'Credenciais inválidas.'
                 else:
-                    return 'Credenciais inválidas.'
+                    cursor.close()
+                    return 'Usuário não encontrado.'
             except Exception as err:
                 return f'Erro ao fazer login: {err}'
 
     def load(self):
         self.app.run(host=self.host, port=self.port)
+
+    def encrypt_password(self, password):
+        salt = bcrypt.gensalt()
+        encrypted_password = bcrypt.hashpw(password.encode('utf-8'), salt)
+
+        return encrypted_password
