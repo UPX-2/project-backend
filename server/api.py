ff82da1c2ac600a2c34f5d2ce139439870f1eb78
João Negreiros <142504941+joao-victor-wls@users.noreply.github.com>
2024-05-21 22:20:13 -0300

    Adicionando token JWT

--- server/api.py
+++ server/api.py
@@ -2,6 +2,7 @@ from flask import Flask, jsonify, request
 from flask_cors import CORS
 from database import *
 import bcrypt
+import jwt
 
 class ServerApi:
     def __init__(self, host='127.0.0.1', port=5000):
@@ -10,8 +11,8 @@ class ServerApi:
         self.app = Flask(__name__)
         CORS(self.app)  # Permitindo solicitações de qualquer origem
         self.connector = MySQLConnector()
-
         self.connector.connect()
+        self.secret_key = 'e6b8e25c0e90427bbf52b9adfd007c0979fa59387d2de55d486d32550a815e6c'
 
         @self.app.route('/')
         def index():
@@ -27,17 +28,14 @@ class ServerApi:
             email = data.get('email')
             password_hash = data.get('password')
             password_hash = self.encrypt_password(password_hash)
-            access_token = "asdasdasdasdasd"
-            refresh_token = "dadasdasdasd"
-
-            print(complete_name)
+            access_token = ""
 
             try:
                 # Inserindo os dados na tabela USERS
                 cursor = self.connector.connection.cursor()
-                query = ("INSERT INTO USERS (COMPLETE_NAME, EMAIL, PASSWORD_HASH, ACCESS_TOKEN, REFRESH_TOKEN) "
-                         "VALUES (%s, %s, %s, %s, %s)")
-                data = (complete_name, email, password_hash, access_token, refresh_token)
+                query = ("INSERT INTO USERS (COMPLETE_NAME, EMAIL, PASSWORD_HASH, ACCESS_TOKEN) "
+                         "VALUES (%s, %s, %s, %s)")
+                data = (complete_name, email, password_hash, access_token)
                 cursor.execute(query, data)
                 self.connector.connection.commit()
                 cursor.close()
@@ -56,26 +54,33 @@ class ServerApi:
             try:
                 # Obtendo os dados do usuário do banco de dados
                 cursor = self.connector.connection.cursor()
-                query = ("SELECT EMAIL, PASSWORD_HASH FROM USERS WHERE EMAIL = %s")
+                query = "SELECT EMAIL, PASSWORD_HASH, ID, COMPLETE_NAME FROM USERS WHERE EMAIL = %s"
                 cursor.execute(query, (email,))
                 user = cursor.fetchone()
 
                 if user:
                     # Obtendo a senha criptografada armazenada no banco de dados
                     senha_criptografada_armazenada = user[1]
-                    
+
                     # Verificando se a senha fornecida corresponde à senha criptografada armazenada
                     if bcrypt.checkpw(password.encode('utf-8'), senha_criptografada_armazenada.encode('utf-8')):
+                        data_token = {
+                            'id': user[2],
+                            'email': user[0],
+                            'nome_completo': user[3],
+                        }
                         cursor.close()
-                        return jsonify({'token': 'token_fixo_qualquer'})
+
+                        token = jwt.encode(data_token, self.secret_key, algorithm='HS256')
+                        return jsonify({'token': token})
                     else:
                         cursor.close()
-                        return 'Credenciais inválidas.'
+                        return jsonify({'error': 'Credenciais inválidas.'}), 401
                 else:
                     cursor.close()
-                    return 'Usuário não encontrado.'
+                    return jsonify({'error': 'Usuário não encontrado.'}), 404
             except Exception as err:
-                return f'Erro ao fazer login: {err}'
+                return jsonify({'error': f'Erro ao fazer login: {err}'}), 500
 
     def load(self):
         self.app.run(host=self.host, port=self.port)
